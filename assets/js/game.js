6b3ae0d22e86ddf22233f95d60a10c0756588111
Austin Graves <austingraves106@aol.com>
2019-08-15 16:37:30 -0500

    Added in the first necessary constant variables for DOM Manipulation.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,19 @@
 console.log('Hello World');
 
+// const variables used for DOM Manipulation.
+const numberYouAreAttemptingToGuess = $('#guessThisNumber');
+const totalWins = $('#totalWins');
+const totalLosses = $('#totalLosses');
+const clickedImages = $('.crystal-img');
+const yourScore = $('#yourScore');
+// End of DOM Manipulation Variables. 
+
+
+
+
+
+
+
+clickedImages.on('click', function() {
+  console.log($(this).attr('value'));
+})
