46a46c3fe499b633e5625fdf9b4d518bee9a2a6b
Austin Graves <austingraves106@aol.com>
2019-08-15 18:37:59 -0500

    Playing with different ways to change the DOM

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,22 @@
 console.log('Hello World');
-
 // const variables used for DOM Manipulation.
-const numberYouAreAttemptingToGuess = $('#guessThisNumber');
-const totalWins = $('#totalWins');
-const totalLosses = $('#totalLosses');
-const clickedImages = $('.crystal-img');
-const yourScore = $('#yourScore');
-// End of DOM Manipulation Variables. 
+// const numberYouAreAttemptingToGuess = $('#guessThisNumber');
+// const totalWins = $('#totalWins');
+// const totalLosses = $('#totalLosses');
+// const clickedImages = $('.crystal-img');
+// const yourScore = $('#yourScore');
+// // End of DOM Manipulation Variables. 
+// clickedImages.on('click', function() {
+//   console.log($(this).attr('value'));
+// })
+
+// Attempting to do this javascript vanilla
+
+// const numberYouAreAttemptingToGuess = document.querySelector('#guessThisNumber');
+// const totalWins = document.querySelector('#totalWins');
+// const totalLosses = document.querySelector('#totalLosses');
+// const clickedImages = document.querySelectorAll('.crystal-img');
+// const yourScore = document.querySelector('#yourScore');
 
 
 
-
-
-
-
-
-clickedImages.on('click', function() {
-  console.log($(this).attr('value'));
-})
